11235956f0b69324c4ff81c25b40fa0e51dc288f
junyong <staccato202@gmail.com>
2026-01-16 16:08:55 +0900

    20260116

---
+++ src/sql/Test1.java
@@ -0,0 +1,13 @@
+package sql;
+
+public class Test1 {
+	public static void main(String[] args) {
+		int tot = 0;
+		for(int i = 0; i <= 100; ++i) {
+			tot += i;
+			if(i % 10 == 0) {
+				System.out.println("1부터" + i + "까지의 합은" + tot + "입니다.");
+			}
+		}
+	}
+}
